1806893778d728d327837da86ec491f92290d987
Anthony <charetteanthony@outlook.com>
2023-12-12 19:41:11 -0500

    FIX: cookies not set on first load

--- controller/userController.js
+++ controller/userController.js
@@ -167,7 +167,7 @@ const currentUser = asyncHandler( async(req,res,next) => {
     if(req.isAuthenticated() && req.user){
         res.status(200).json(req.user);
     } else {
-        res.status(200).json({ message: 'New customer' });
+        res.status(404).json({ message: 'New customer' });
     }
 });
 
