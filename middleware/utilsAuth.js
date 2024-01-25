b0d52e5a393162f88b0a07d9b0dfd687b25f7d01
Tony Char3 <tonyc6731@gmail.com>
2024-01-24 23:39:41 -0500

    UPDATE: fixing redirect urls

--- middleware/utilsAuth.js
+++ middleware/utilsAuth.js
@@ -30,7 +30,7 @@ const cookiesIdentityVerification = async(req,res) => {
  * Verify authencation token
  */
 const AuthVerification = (req,res,next) => {
-    console.log("Verification: ", req);
+    console.log("Verification: ", req.isAuthenticated());
     if(req.isAuthenticated()){
         next()
     } else {
